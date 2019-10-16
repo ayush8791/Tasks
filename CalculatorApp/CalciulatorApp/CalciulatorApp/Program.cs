7ced4b1086b514753dcc6f90e2eab25c94300348
Ayush Garg <ayush8791@gmail.com>
2019-10-16 15:55:05 +0530

    Add files via upload

---
+++ CalculatorApp/CalciulatorApp/CalciulatorApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathOperations;
+
+namespace CalciulatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Two integer values for possible operations add and divide");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter operation add(a) \n subtract(s) \n multiply(m) \n divide(d) \n ans?");
+            string op = Console.ReadLine();
+            if (op == "a")
+                Console.WriteLine(MathOp.add(a, b));
+            else if (op == "d")
+            {
+                if (MathOp.divide(a, b) != null)
+                {
+                    Console.WriteLine(MathOp.divide(a, b));
+                }
+
+
+            }
+            else if (op == "m")
+            {
+                Console.WriteLine(MathOp.multiply(a, b));
+            }
+            else if (op == "s")
+            {
+                Console.WriteLine(MathOp.subtract(a, b));
+            }
+            else
+                Console.WriteLine("Not Valid!");
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadKey(); 
+        }
+
+    }
+}
---
+++ CalculatorApp/CalciulatorApp/MathOperations/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public static  class MathOp
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int? divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("invalid : divide can't take 0 as divisor");
+                return null;
+            }
+
+            return a / b;
+
+        }
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
