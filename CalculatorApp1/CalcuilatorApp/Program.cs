eeade6a1985efcf60616598f7e5948fb86dd6a1e
Ayush Garg <ayush8791@gmail.com>
2019-10-17 11:16:29 +0530

    Add files via upload

---
+++ CalculatorApp1/CalcuilatorApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathOperations;
+
+namespace CalciulatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOp Operator= new MathOp();
+            Console.WriteLine("Enter Two integer values for possible operations add and divide");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter operation add(a) \n subtract(s) \n multiply(m) \n divide(d) \n ans?");
+            string op = Console.ReadLine();
+            if (op == "a")
+                Console.WriteLine(Operator.add(a, b));
+            else if (op == "d")
+            {
+                if (Operator.divide(a, b) != null)
+                {
+                    Console.WriteLine(Operator.divide(a, b));
+                }
+
+
+            }
+            else if (op == "m")
+            {
+                Console.WriteLine(Operator.multiply(a, b));
+            }
+            else if (op == "s")
+            {
+                Console.WriteLine(Operator.subtract(a, b));
+            }
+            else
+                Console.WriteLine("Not Valid!");
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadKey(); 
+        }
+
+    }
+}
---
+++ CalculatorApp1/MathOperations/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public interface MathOpInterface
+    {
+        int add(int a, int b);
+        int? divide(int a, int b);
+        int multiply(int a, int b);
+        int subtract(int a, int b);
+
+    
+    }
+    public class MathOp:MathOpInterface
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public int? divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("invalid : divide can't take 0 as divisor");
+                return null;
+            }
+
+            return a / b;
+
+        }
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
